b6fd0d80749dfeb039fb0b9d261f01980f7c3a75
yusrilarzaqi <70110733+yusrilarzaqi@users.noreply.github.com>
2021-12-15 17:31:28 +0700

    add new file pertemuan14

--- pertemuan14/functions.php
+++ pertemuan14/functions.php
@@ -125,4 +125,36 @@ function cari($keyword){
   return query($query);
 }
 
+
+function registrasi($data){
+  global $conn;
+
+  $username = strtolower(stripslashes($data['username']));
+  $password = mysqli_real_escape_string($conn, $data['password']);
+  $password2 = mysqli_real_escape_string($conn,$data['password2']);
+
+  // cek apakah username sudah ada
+  $result = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
+
+  if (mysqli_fetch_assoc($result)){
+    echo "<script> alert('username yang dipilih sudah ada, silahkan pilih yang lain'); </script> ";
+    return false;
+  }
+
+  // confirm 
+  if( $password !== $password2){
+    echo "<script> alert('konfirmasi password tidak sesuai');</script>";
+    return false;
+  }
+
+  // enkripsi password
+  $password = password_hash($password, PASSWORD_DEFAULT);
+
+  // tambahkan ke database
+  mysqli_query($conn, "INSERT INTO user VALUES('', '$username', '$password')");
+
+
+
+  return mysqli_affected_rows($conn);
+}
 ?>
